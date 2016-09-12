f42e70e50f5235234371dde605646a235fdc83f7
Rob Sturcke <rs@rsvision.com>
2016-09-12 16:46:57 -0400

    markup complete along with css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def markdown(text)
-    Redcarpet.new(text, :hard_wrap, :filter_html, :autolink, :no_intraemphasis).to_html.html_safe
+    # choose a rednerer and pass the options (renderer is either Render::XML or Render::HTML)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true)
+    # create a new instance of markdown and pass the renderer
+    md = Redcarpet::Markdown.new(renderer)
+    # use the instance to render the text
+    md.render(text).html_safe
   end
 end
