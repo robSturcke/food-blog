9c448dcc2e78afb2ec40af9b1c1c40720102bb3b
Rob Sturcke <rs@rsvision.com>
2016-09-13 00:06:26 -0400

    precompile and CSS along with image fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
 
-  has_attached_file :picture, styles: { medium: "300x300", thumb: "100x100" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { medium: "500x500", thumb: "100x100", large: "900x900 " }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
